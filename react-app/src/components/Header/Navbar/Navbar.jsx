d2a2b4a2f606359c54c029db0d11709ebf5f64e0
unknown <andjeybur@gmail.com>
2022-10-29 23:15:30 +0300

    first commit

---
+++ react-app/src/components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import store from "../../../store/store";
+import {observer} from "mobx-react-lite";
+
+const Navbar = () => {
+    const scrollToCategory = (id) => {
+        const link = document.getElementById("category" + id)
+        link.scrollIntoView({behavior: "smooth"})
+    }
+
+    return (
+        <div className="top__menu">
+            {store.listCategory.map((item) => {
+                return (<div className="menu__item" key={item.id}>
+                    <button onClick={() => scrollToCategory(item.id)}>{item.title}</button>
+                </div>);
+            })}
+        </div>
+    );
+}
+
+export default observer(Navbar);
